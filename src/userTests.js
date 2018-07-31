1d029ca09dde62c0debaee6266c3cab0629ae9ad
mhudnell <maxhudnell@gmail.com>
2018-07-31 18:06:41 -0400

    catch error when user test folder doesn't exist

--- src/userTests.js
+++ src/userTests.js
@@ -1,8 +1,12 @@
 const testStore = require('testStorePath'); // refers to an alias specified in webpack config
 
 // find and require all test files
-let r = require.context('usrTestsPath', true, /\.test\.js$/);
-r.keys().forEach(r)
+try {
+  let r = require.context('usrTestsPath', true, /\.test\.js$/);
+  r.keys().forEach(r)
+} catch(err) {
+  console.log("Can't resolve 'usrTestsPath': Make sure your test files are in a folder named 'hubble-tests' located in the root directory of your application.");
+}
 
 let testData = testStore.getTests();  // returns all test info in a javascript object
 
